218a853890e85a27bc67a1527f71dbea7f71115a
asbelnascimentodev <asbel.nascimento157@gmail.com>
2026-03-04 21:29:42 -0300

    feat: set up initial portfolio website structure with navigation, smooth scrolling, and hero section.

--- src/App.tsx
+++ src/App.tsx
@@ -183,14 +183,14 @@ const App = () => {
                         SYSTEMS STATUS: OPTIMAL
                     </div>
                     
-                    <h1 className="text-[10px] sm:text-xs md:text-xl text-cyber-blue font-orbitron tracking-[0.5em] sm:tracking-[1em] mb-4 opacity-70">
+                    <h1 className="text-[3px] xs:text-[6px] sm:text-xs md:text-xl text-cyber-blue font-orbitron tracking-[0.05em] sm:tracking-[1em] mb-4 opacity-70">
                         WELCOME_PROTOCOL
                     </h1>
                     
                     <div className="mb-8">
                         <GradualSpacing
                             text="ASBEL NASCIMENTO"
-                            className="text-2xl sm:text-6xl md:text-8xl lg:text-9xl font-orbitron font-black text-white leading-none tracking-tighter"
+                            className="text-[4px] xs:text-xs sm:text-4xl md:text-8xl lg:text-9xl font-orbitron font-black text-white leading-none tracking-tighter"
                         />
                     </div>
 
